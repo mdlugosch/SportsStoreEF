31624d9573add46c474052e062d928b7b34ee4fa
Michael Dlugosch <michael.dlugosch@web.de>
2018-12-18 15:07:15 +0100

    Test

--- SportsStoreEF/Models/CategoryRepository.cs
+++ SportsStoreEF/Models/CategoryRepository.cs
@@ -35,7 +35,14 @@ namespace SportsStoreEF.Models
 
         public void UpdateCategory(Category category)
         {
-            context.Categories.Update(category);
+            //Category query = context.Categories.Find(category.Id);
+            //Category query = (from c in Categories where c.Id == category.Id select c).FirstOrDefault();
+            //query.Name = category.Name;
+            //query.Description = category.Description;
+
+            (context.Categories.Find(category.Id)).Name=category.Name;
+            (context.Categories.Find(category.Id)).Description = category.Description;
+            //context.Categories.Update(category);
             context.SaveChanges();
         }
     }
