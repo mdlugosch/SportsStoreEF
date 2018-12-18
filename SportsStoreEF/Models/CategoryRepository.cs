62b60114169572e2b6bd7fdbf512bf643bcc3a17
Michael Dlugosch <michael.dlugosch@web.de>
2018-12-18 15:07:27 +0100

    test

--- SportsStoreEF/Models/CategoryRepository.cs
+++ SportsStoreEF/Models/CategoryRepository.cs
@@ -38,7 +38,7 @@ namespace SportsStoreEF.Models
             //Category query = context.Categories.Find(category.Id);
             //Category query = (from c in Categories where c.Id == category.Id select c).FirstOrDefault();
             //query.Name = category.Name;
-            //query.Description = category.Description;
+            //query.Description = category.Description; 
 
             (context.Categories.Find(category.Id)).Name=category.Name;
             (context.Categories.Find(category.Id)).Description = category.Description;
