2431a3009dc736b5935f0a1b1a639e68fe7ce0bd
Dlugosch <Michael.Dlugosch@web.de>
2019-01-18 11:51:26 +0100

    Kapitel 8 Final

--- SportsStoreEF/Models/DataContext.cs
+++ SportsStoreEF/Models/DataContext.cs
@@ -9,7 +9,7 @@ namespace SportsStoreEF.Models
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> opts) : base(opts) { }
-
+         
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
